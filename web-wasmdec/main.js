b5d301b983f7619d6cbe75e7611630c8170640f9
wwwg <joshpauten@gmail.com>
2018-09-17 14:27:07 -0500

    initialize output editor as C

--- web-wasmdec/main.js
+++ web-wasmdec/main.js
@@ -2,5 +2,7 @@
     window.onload = () => {
         let inEditor = ace.edit("input");
         inEditor.session.setMode("ace/mode/lisp");
+        let outEditor = ace.edit('output');
+        outEditor.session.setMode("ace/mode/c_cpp");
     }
 })();
