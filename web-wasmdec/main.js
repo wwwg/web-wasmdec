1d5f462caa5b6b0dd9e211c1ddb5bc75d13be89d
wwwg <joshpauten@gmail.com>
2018-09-17 14:35:12 -0500

    set default editor values

--- web-wasmdec/main.js
+++ web-wasmdec/main.js
@@ -1,7 +1,19 @@
 (() => {
+    const defaultInput = `(module
+	(func $addTwo (param i32 i32) (result i32)
+		(return
+			(i32.add (get_local 0) (get_local 1))
+		)
+	)
+	(export "addTwo" $addTwo)
+)`;
+    const defaultOutput = `int fn_addTwo(int arg0, int arg1) {
+	return arg0 + arg1;
+}`;
     window.onload = () => {
         let inEditor = ace.edit("input");
         inEditor.session.setMode("ace/mode/lisp");
+        inEditor.setValue(defaultInput, -1);
         // setup output
         let outEditor = ace.edit('output');
         outEditor.session.setMode("ace/mode/c_cpp");
@@ -13,5 +25,6 @@
         outEditor.renderer.$cursorLayer.element.style.opacity = 0;
         outEditor.commands.commmandKeyBinding = {};
         outEditor.textInput.getElement().disabled = true;
+        outEditor.setValue(defaultOutput, -1);
     }
 })();
