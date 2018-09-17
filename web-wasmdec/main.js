6149c2974e68471dc987b8548f284eed4ada3a94
wwwg <joshpauten@gmail.com>
2018-09-17 14:32:26 -0500

    make output readonly

--- web-wasmdec/main.js
+++ web-wasmdec/main.js
@@ -2,7 +2,16 @@
     window.onload = () => {
         let inEditor = ace.edit("input");
         inEditor.session.setMode("ace/mode/lisp");
+        // setup output
         let outEditor = ace.edit('output');
         outEditor.session.setMode("ace/mode/c_cpp");
+        outEditor.setOptions({
+            readOnly: true,
+            highlightActiveLine: false,
+            highlightGutterLine: false
+        });
+        outEditor.renderer.$cursorLayer.element.style.opacity = 0;
+        outEditor.commands.commmandKeyBinding = {};
+        outEditor.textInput.getElement().disabled = true;
     }
 })();
